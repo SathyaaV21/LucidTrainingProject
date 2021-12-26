84514c23b727081b2bc997ad91ad5b91fa228487
SathyaaV21 <sathyaa210200@gmail.com>
2021-12-26 22:40:46 +0530

    Added @service annotation to service class

--- TrainingProject1/src/main/java/com/example/demo/service/RoleService.java
+++ TrainingProject1/src/main/java/com/example/demo/service/RoleService.java
@@ -3,16 +3,18 @@ package com.example.demo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.model.RoleModel;
 import com.example.demo.repository.RoleRepository;
-
+@Service
 public class RoleService {
 	
 @Autowired
 private RoleRepository roleRepo;
-public RoleModel saveRole(RoleModel newrole) {
-	return roleRepo.save(newrole);
+public String saveRole(RoleModel newrole) {
+	roleRepo.save(newrole);
+	return "In Service";
 }
 public List<RoleModel>viewRoles(){
 	
