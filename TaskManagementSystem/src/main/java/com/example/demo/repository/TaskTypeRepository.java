dd46b6b04fac15ab88b14008c85e23212fabb641
ramapriya0023 <64014949+ramapriya0023@users.noreply.github.com>
2022-01-08 13:12:09 +0530

    Delete TaskManagementSystem directory

--- TaskManagementSystem/src/main/java/com/example/demo/repository/TaskTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.demo.model.TaskTypeModel;
-
-public interface TaskTypeRepository extends MongoRepository<TaskTypeModel,Integer> {
-
-}
