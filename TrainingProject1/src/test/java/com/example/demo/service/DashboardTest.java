96f150a581ae4a38b10b414b2811cf72b87e2646
ritikam15 <75538165+ritikam15@users.noreply.github.com>
2022-01-19 23:42:50 +0530

    no message

--- TrainingProject1/src/test/java/com/example/demo/service/DashboardTest.java
+++ TrainingProject1/src/test/java/com/example/demo/service/DashboardTest.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 import java.util.stream.Collectors;
@@ -13,23 +14,55 @@ import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import com.example.demo.model.RTM;
+import com.example.demo.model.TaskModel;
+import com.example.demo.model.TestCaseCount;
+import com.example.demo.model.DefectCount;
 import com.example.demo.model.File;
 import com.example.demo.service.DashboardService;
 
 import com.example.demo.exception.ProjectNotFoundException;
 
+@SpringBootTest
 public class DashboardTest {
 	
 	@SpyBean
 	private MongoTemplate mongoTemplate;
 	
 	@Autowired
-	private DashboardService dashboardservice;
+	private DashboardService dashboardService;
 	
 	
 	@Test
 	public void getRTMTest() throws ProjectNotFoundException {
-		dashboardservice.getRTM();
+		assertEquals(dashboardService.getRTM().size(),5);
 		
 	}
+	
+	@Test
+	public void getTestCaseProgressTest() {
+	  assertTrue(dashboardService.getTestCaseProgress().get(0) instanceof
+	  TestCaseCount);
+	  
+	  }
+	 
+	@Test
+	public void getDefectsProgressTest() {
+	  assertTrue(dashboardService.getDefectsProgress().get(0) instanceof DefectCount);
+	  
+	  }
+	@Test
+	public void getTestCaseProgressbyProjIdTest() throws ProjectNotFoundException {
+	  assertTrue(dashboardService.getTestCaseProgressbyProjId("Prj2").get(0) instanceof TestCaseCount);
+	  
+	  }
+	
+		/*
+		 * @Test public void getOpenTestCaseCountbyRequirementIdTest() {
+		 * assertTrue(dashboardService.getOpenTestCaseCountbyRequirementId("Prj4Req1")
+		 * instanceof int);
+		 * 
+		 * }
+		 */
+	
+	
 }
