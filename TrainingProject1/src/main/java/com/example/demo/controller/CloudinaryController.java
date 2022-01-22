d9270a8ae2efb2fc1fb0ca0f88f839fe7337a30a
ritikam15 <75538165+ritikam15@users.noreply.github.com>
2022-01-22 17:01:08 +0530

    no message

--- TrainingProject1/src/main/java/com/example/demo/controller/CloudinaryController.java
+++ TrainingProject1/src/main/java/com/example/demo/controller/CloudinaryController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -84,7 +85,7 @@ public class CloudinaryController {
 	 * @param defect_id as HashMap.
 	 * @return String stating the message.
 	 */
-
+	@PreAuthorize("hasAuthority('ROLE_TESTER')")
 	@DeleteMapping("/deleteallfilesbyid")
 	public String deleteAllFiles(@RequestBody HashMap<String, String> dataHashMap) {
 		return cloudinaryfileservice.deleteAllFiles(dataHashMap.get("defect_id"));
